d310cf37a65ad6457e6f1aff3e45dba133cba161
qNOVAq <s4lmsheikh@gmail.com>
2020-08-12 02:09:48 +0300

    added new csv file

---
+++ Basics/practice_1.java
@@ -0,0 +1,9 @@
+public class practice_1{
+    public static void main(String []args) {
+        int number = 17;
+        String name = "Salman";
+
+        System.out.println("Hello my name is " + name);
+        System.out.println("I am " + number + " years old");
+    }
+}
